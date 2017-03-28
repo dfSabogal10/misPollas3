c83c4c0eb1f4fe36bbefc6ae908186954530b1ba
jecamargo10 <javier.camargo@fobsoftware.com>
2017-03-27 23:06:24 -0500

    cambio render perchito
    asd

--- imports/ui/components/Fixtures.jsx
+++ imports/ui/components/Fixtures.jsx
@@ -12,74 +12,77 @@ export default class Fixtures extends Component {
 	}
 
 
-  render(){
-    if (this.props.fixture.status ==="FINISHED")
-    {
-      return (
+    render(){
+      if (this.props.fixture.status ==="FINISHED")
+      {
+        return (
 
-      <table className="fixes">
-      <thead>
-       <tr>
-           <th>Local</th>
-           <th>Visitante</th>
-           <th>Fecha</th>
-           <th>Jornada</th>
-           <th> </th>
-
-
-
-       </tr>
-      </thead>
-      <tbody>
-       <tr>
-           <td>{this.props.fixture.homeTeamName}</td>
-           <td>{this.props.fixture.awayTeamName}</td>
-           <td>{this.props.fixture.date}</td>
-           <td>{this.props.fixture.matchday}</td>
-
-       </tr>
-
-      </tbody>
-
-      </table>
-);
-    }
-    else {
-
-    return (
-              <table className="fixes">
-              <thead>
-               <tr>
-                   <th>Local</th>
-                   <th>Visitante</th>
-                   <th>Fecha</th>
-                   <th>Jornada</th>
-                   <th>Goles Local</th>
-                   <th>Goles Visitante</th>
-                   <th> </th>
-
-               </tr>
-           </thead>
-           <tbody>
-               <tr>
-                   <td>{this.props.fixture.homeTeamName}</td>
-                   <td>{this.props.fixture.awayTeamName}</td>
-                   <td>{this.props.fixture.date}</td>
-                   <td>{this.props.fixture.matchday}</td>
-                  <td> <input type="number" name="visitante" /></td>
-                  <td> <input type="number" name="local" /></td>
-<td>
-                   <button onClick={this.apostar.bind(this)}>Apostar</button>
-</td>
+        <table className="fixes">
+        <thead>
+         <tr>
+             <th>Local</th>
+             <th>Visitante</th>
+             <th>Fecha</th>
+             <th>Jornada</th>
+             <th></th>
 
 
-               </tr>
 
-           </tbody>
+         </tr>
+        </thead>
+        <tbody>
+         <tr>
+             <td>{this.props.fixture.homeTeamName}</td>
+             <td>{this.props.fixture.awayTeamName}</td>
+             <td>{this.props.fixture.date}</td>
+             <td>{this.props.fixture.matchday}</td>
+
+         </tr>
+
+        </tbody>
 
         </table>
-        );
+  );
       }
+      else {
+
+      return (
+                <table className="fixes">
+                <thead>
+                 <tr>
+                     <th>Local</th>
+                     <th>Visitante</th>
+                     <th>Fecha</th>
+                     <th>Jornada</th>
+                     <th>Goles Local</th>
+                     <th>Goles Visitante</th>
+                     <th> </th>
+
+                 </tr>
+             </thead>
+             <tbody>
+                 <tr>
+                     <td>{this.props.fixture.homeTeamName}</td>
+                     <td>{this.props.fixture.awayTeamName}</td>
+                     <td>{this.props.fixture.date}</td>
+                     <td>{this.props.fixture.matchday}</td>
+                    <td> <input type="number" ref="visitante" /></td>
+                    <td> <input type="number" ref="local" /></td>
+  <td>
+                     <button onClick={this.apostar.bind(this)}>Apostar</button>
+  </td>
+  <td>
+                     <button onClick={this.apostar.bind(this)}>SHARE</button>
+  </td>
+
+
+                 </tr>
+
+             </tbody>
+
+          </table>
+          );
+        }
 
 
 
@@ -90,5 +93,5 @@ export default class Fixtures extends Component {
 
 
 
+    }
   }
-}
